9cb9bbffb7e0e5d626a8d350d3112e8478b39f59
Leonardo da Silva <lslv1243@gmail.com>
2025-01-14 09:59:55 -0300

    add and use dependency with existing update

--- DependabotPOC/ContentView.swift
+++ DependabotPOC/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Leonardo da Silva on 14/01/25.
 //
 
+import Algorithms
 import SwiftUI
 
 struct ContentView: View {
@@ -16,6 +17,12 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            let numbers = [10, 20, 30, 10, 40, 40, 10, 20]
+            let chunks = numbers.chunked(by: { $0 <= $1 })
+            // [[10, 20, 30], [10, 40, 40], [10, 20]]
+            print(chunks)
+        }
     }
 }
 
--- Packages/ExternalPackages/Package.swift
+++ Packages/ExternalPackages/Package.swift
@@ -5,20 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "ExternalPackages",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ExternalPackages",
             targets: ["ExternalPackages"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMinor(from: "1.0.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ExternalPackages"),
-        .testTarget(
-            name: "ExternalPackagesTests",
-            dependencies: ["ExternalPackages"]
+            name: "ExternalPackages",
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+            ]
         ),
     ]
 )
