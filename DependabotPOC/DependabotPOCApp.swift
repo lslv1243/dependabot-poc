749efd0e6c01a25f11444395f4c8220106aea12c
Leonardo da Silva <lslv1243@gmail.com>
2025-01-14 09:48:42 -0300

    Initial Commit

---
+++ DependabotPOC/DependabotPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependabotPOCApp.swift
+//  DependabotPOC
+//
+//  Created by Leonardo da Silva on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DependabotPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
